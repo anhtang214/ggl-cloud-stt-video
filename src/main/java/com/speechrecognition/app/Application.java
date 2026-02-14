4faf3e3dafda968855c0f314eaf3c24ecc2d8eba
Thuy Anh <160872725+aina2104@users.noreply.github.com>
2026-02-14 16:14:06 +1000

    Modify html&css for upload-video form, add base code for /storage, FileUploadController, StorageProperties

--- src/main/java/com/speechrecognition/app/Application.java
+++ src/main/java/com/speechrecognition/app/Application.java
@@ -1,16 +1,28 @@
 package com.speechrecognition.app;
 
+import com.speechrecognition.app.config.StorageProperties;
+import com.speechrecognition.app.service.storage.StorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import com.speechrecognition.app.model.User;
 import com.speechrecognition.app.repository.UserRepository;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
