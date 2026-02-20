41379826f92d69b390070ed7c843ec8f50f91131
Thuy Anh <160872725+aina2104@users.noreply.github.com>
2026-02-21 04:44:49 +1000

    Modify Application to match previous commits

--- src/main/java/com/speechrecognition/app/Application.java
+++ src/main/java/com/speechrecognition/app/Application.java
@@ -1,14 +1,10 @@
 package com.speechrecognition.app;
 
 import com.speechrecognition.app.config.StorageProperties;
-import com.speechrecognition.app.service.storage.StorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import com.speechrecognition.app.model.User;
-import com.speechrecognition.app.repository.UserRepository;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -16,13 +12,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    CommandLineRunner init(StorageService storageService) {
-        return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
-        };
     }
 }
